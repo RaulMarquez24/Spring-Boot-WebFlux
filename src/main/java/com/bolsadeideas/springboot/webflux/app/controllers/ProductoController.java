f9c96fab904a3282815553e0fec5b224a84ab227
Raúl Márquez <raulmu785@gmail.com>
2023-11-03 11:14:37 +0100

    modificando stream reactivo

--- src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
+++ src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
@@ -1,10 +1,11 @@
 package com.bolsadeideas.springboot.webflux.app.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import com.bolsadeideas.springboot.webflux.app.models.dao.ProductoDao;
 import com.bolsadeideas.springboot.webflux.app.models.documents.Producto;
 
@@ -12,14 +13,21 @@ import reactor.core.publisher.Flux;
 
 @Controller
 public class ProductoController {
-    
+
     @Autowired
     private ProductoDao productoDao;
+
+    private static final Logger log = LoggerFactory.getLogger(ProductoController.class);
 
     @GetMapping({"/listar", "/"})
     public String listar(Model model) {
 
-        Flux<Producto> productos = productoDao.findAll();
+        Flux<Producto> productos = productoDao.findAll().map(producto -> {
+            producto.setNombre(producto.getNombre().toUpperCase());
+            return producto;
+        });
+
+        productos.subscribe(prod -> log.info(prod.getNombre()));
 
         model.addAttribute("productos", productos);
         model.addAttribute("titulo", "Listado de productos");
